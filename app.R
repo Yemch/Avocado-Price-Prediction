09599d3753a4133f26b572ad4e601fc8d4247ed7
Yemch <954747896@qq.com>
2020-12-09 11:01:59 -0500

    add plots according to avocado types

--- app.R
+++ app.R
@@ -6,8 +6,7 @@ library(shiny)
 library(shinydashboard)
 
 avocado = read.csv("./avocado_clean.csv")
-avocadodate = read.csv("./avocado_clean.csv")
-avocadodate$Date = as.Date(avocadodate$Date)
+avocado$Date = as.Date(avocado$Date)
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
@@ -42,26 +41,27 @@ ui <- dashboardPage(
         tabItems(
             # ----- The first page-------
             tabItem(tabName = "basic",
-                    h2("Avovado Prices and Volumns"),
+                    h2("Avovado Prices and Volumns by County Level"),
                     fluidRow(
-                        box(title = "Choose a county",  status = "primary", solidHeader = F,
-                          
+                        box(title = "Choose a county",  status = "warning", solidHeader = F,
                             selectInput(inputId = "state", label = "State", choices = avocado$State),   
                             uiOutput("select")
-                            
                             ), # box 
                         
                         box(title = "Choose price or volumn", status = "warning", solidHeader = F,
                             radioButtons(inputId = "pricevolumn", label = NULL, choices = c("Price"="AveragePrice","Volume"="Total.Volume")), 
                             selectInput(inputId = "date", label = "Date", choices = avocado$Date)
-                            ) # box 
+                            ) # box
+                        
                     ), # fluidRow
                     
                     fluidRow(
-                        box(plotOutput(outputId = "line")), # box
+                        box(plotOutput(outputId = "line_org"),
+                            plotOutput(outputId = "line_con")), # box
                         
-                        box(plotOutput(outputId = "bar"))
-                        
+                        box(plotOutput(outputId = "bar_org"),
+                            plotOutput(outputId = "bar_con")
+                            )
                         
                     ) # fluidRow
                     
@@ -110,7 +110,6 @@ ui <- dashboardPage(
                     which comes from the US Department of Agriculture website. 
                     The data can be downloaded in .xls format. The following links are the sources of data:"),
                     p("https://www.kaggle.com/neuromusic/avocado-prices"),
-                    p("https://www.californiaavocadogrowers.com/industry/industry-statistical-data"),
                     p("https://www.ers.usda.gov/data-products/food-environment-atlas/data-access-and-documentation-downloads/"),
 
                     br(),
@@ -141,37 +140,74 @@ server <- function(input, output) {
     
     ###Tab 1 - basic statistics descriptions
     #reactive
-    county <- reactive(avocadodate %>% filter(State %in% input$state & County %in% input$county))
-    
     plot_y <- reactive({
-        if(input$pricevolumn=="AveragePrice"){plot_y="Avocado Price"}
+        if(input$pricevolumn=="AveragePrice"){plot_y="Avocado Price (Dollar)"}
         else{plot_y="Total Volume of Avocado"}
     })
     
-    bar <- reactive(avocado %>% 
-                        filter(Date %in% input$date) 
-    )
+    title = reactive({
+        if(input$pricevolumn=="AveragePrice"){title ="The Average Price of a Single Avocado"}
+        else{title ="Total Number of Avocados Sold"}
+    })
+    
     
     ##price/volume over time group by county  (line plot)
-    output$line <- renderPlot({
-        county() %>%
-            ggplot() +
-            geom_line(aes_string(x="Date", y=input$pricevolumn, group="County", col="County"))+ 
-            theme_light() +
-            scale_x_date(date_breaks = "3 month", date_labels = "%b") +
-            labs(title = paste(plot_y(),"Over Time"), y=plot_y())
-    }) 
-  
-    ##volume/price in different counties at a certain time point (barplot)
-    output$bar <- renderPlot({
-        bar() %>%
-            ggplot(aes_string(x="County", y=input$pricevolumn))+
-            geom_bar(stat="identity")+
-            coord_flip()+
-            labs(title=paste(plot_y(),"on", input$date), y=plot_y())
+    output$line_org <- renderPlot({
+        
+    avocado %>% filter(State %in% input$state & County %in% input$county & type == "organic") %>%  
+        ggplot() +
+        geom_line(aes_string(x="Date", y=input$pricevolumn, group="County"), color = "darkolivegreen4")+
+        ylim(0,3.25) +
+        theme_light() +
+        scale_x_date(date_breaks = "3 month", date_labels = "%b") +
+        labs(title = paste(title(),"(Organic) Over Time"), y=plot_y())
     }) 
     
-}
+    output$line_con <- renderPlot({
+        
+        avocado %>% filter(State %in% input$state & County %in% input$county & type == "conventional") %>%  
+            ggplot() +
+            geom_line(aes_string(x="Date", y=input$pricevolumn, group="County"), color = "darkolivegreen4")+ 
+            ylim(0,3.25) +
+            theme_light() +
+            scale_x_date(date_breaks = "3 month", date_labels = "%b") +
+            labs(title = paste(title(),"(Conventional) Over Time"), y=plot_y())
+    }) 
+  
+    #volume/price in different counties at a certain time point (barplot)
+    output$bar_org <- renderPlot({
+    
+    avocado = avocado %>% filter(Date %in% as.Date(input$date)) %>% filter(type == "organic")
+    avocado$County = reorder(avocado$County, avocado[,input$pricevolumn]) 
+    
+    avocado %>%
+        ggplot(aes_string(x= "County", y=input$pricevolumn, fill = input$pricevolumn) )+
+        geom_bar(stat="identity") +
+        coord_flip()+
+        ylim(0,3.25) +
+#        scale_y_continuous(expand = c(0.01,0.01)) +
+        scale_fill_gradient(low = "lightgoldenrod", high = "darkolivegreen3") + 
+        theme(legend.position = "none", panel.background = element_blank(), axis.line = element_line()) +
+        labs(title=paste(title(),"(Organic) on", input$date), y=plot_y())
+    }) # renderPlot
+    
+    output$bar_con <- renderPlot({
+        
+        avocado = avocado %>% filter(Date %in% as.Date(input$date)) %>% filter(type == "conventional")
+        avocado$County = reorder(avocado$County, avocado[,input$pricevolumn]) 
+        
+        avocado %>%
+            ggplot(aes_string(x= "County", y=input$pricevolumn, fill = input$pricevolumn) )+
+            geom_bar(stat="identity") +
+            coord_flip() +
+            ylim(0,3.25) +
+  #          scale_y_continuous(expand = c(0.01,0.01)) +
+            scale_fill_gradient(low = "lightgoldenrod", high = "darkolivegreen3") + 
+            theme(legend.position = "none", panel.background = element_blank(), axis.line = element_line()) +
+            labs(title=paste(title(),"(Conventional) on", input$date), y=plot_y())
+    })
+    
+} # server
 
 # Run the application 
 shinyApp(ui = ui, server = server)
