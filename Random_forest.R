f386d6aeaab7122c346a8a522dd05bc570bf0be3
Nazleen Khan <nak316@mail.harvard.edu>
2020-12-09 22:03:10 -0500

    Version date 12/2020 @ 10:03 PM ET

--- Random_forest.R
+++ Random_forest.R
@@ -1,3 +1,11 @@
+---
+  title: "Random forest"
+author: "Nazleen Khan"
+date: "12/7/2020"
+output: html_document
+---
+  
+  ## Loading necessary packages
 library(tidyverse)
 library(gamlr)
 library(Matrix)
@@ -7,35 +15,38 @@ library(MASS)
 library(knitr)
 library(caTools)
 
+## Extending inference to whole United States in 2017
+
 ## Pulling in the wrangled data and creating the training and testing datasets
 avocado_clean <- read.csv("avocado_clean.csv") # 10,140 observations of 306 variables
-avocado_ca_2017 <- avocado_clean %>%
-  filter(State == "CALIFORNIA" & year == 2017) %>% 
+avocado_2017 <- avocado_clean %>%
+  filter(year == 2017 & County != "roanoke" & County != "stlouis") %>% 
   dplyr::select(c("AveragePrice", "Total.Volume", "Total.Bags", "type",
                   "County", "Population_Estimate_2016", "PCT_LACCESS_POP15",
                   "GROCPTH16", "SUPERCPTH16", "CONVSPTH16", "SPECSPTH16",
                   "SNAPSPTH12", "WICSPTH16", "FFRPTH16", "FSRPTH16", 
-                  "PC_FFRSALES12", "PC_FSRSALES12", "PCH_FDPIR_12_15",
+                  "PC_FFRSALES12", "PC_FSRSALES12",
                   "SODATAX_STORES14", "SODATAX_VENDM14", "CHIPSTAX_STORES14",
-                  "CHIPSTAX_VENDM14", "FOOD_TAX14",
-                  "DIRSALES_FARMS12", "PC_DIRSALES07", "VEG_FARMS12",
-                  "FRESHVEG_ACRES12", "ORCHARD_FARMS12", "BERRY_FARMS12",
-                  "SLHOUSE12", "GHVEG_FARMS12", "CSA12", "AGRITRSM_OPS12",
-                  "FARM_TO_SCHOOL15", "PCT_DIABETES_ADULTS13", "RECFACPTH16",
-                  "PCT_NHWHITE10", "PCT_NHBLACK10", "PCT_HISP10", "PCT_NHASIAN10",
-                  "PCT_NHNA10", "PCT_NHPI10", "PCT_65OLDER10", "PCT_18YOUNGER10",
-                  "MEDHHINC15", "POVRATE15", "METRO13", "POPLOSS10"))
-# Train and test prediction model using 2017 observations from California 
-# 424 observations of 48 variables 
+                  "CHIPSTAX_VENDM14", "PCH_FDPIR_12_15", "FOOD_TAX14", "METRO13",
+                  "DIRSALES_FARMS12", "VEG_FARMS12", "ORCHARD_FARMS12",
+                  "BERRY_FARMS12", "SLHOUSE12", "GHVEG_FARMS12", "CSA12",
+                  "AGRITRSM_OPS12","FARM_TO_SCHOOL15", "PCT_DIABETES_ADULTS13",
+                  "RECFACPTH16", "PCT_NHWHITE10", "PCT_NHBLACK10", "PCT_HISP10",
+                  "PCT_NHASIAN10", "PCT_NHNA10", "PCT_NHPI10", "PCT_65OLDER10",
+                  "PCT_18YOUNGER10", "MEDHHINC15", "POVRATE15", "POPLOSS10"))
+# Exclude Roanoke county and St. Louis because they have missing values across several important predictors
+# Train and test prediction model using 2017 observations 
+# 2,968 observations of 46 variables 
 # Pre-selected variables without substantial missingness or correlation from subject matter knowledge
-set.seed(101)
-sample = sample.split(avocado_ca_2017, SplitRatio = 0.70)
-train = subset(avocado_ca_2017, sample == TRUE)
-test = subset(avocado_ca_2017, sample == FALSE) 
-# Randomly split 2017 California data into 70:30 training and testing datasets
+set.seed(610)
+sample = sample.split(avocado_2017, SplitRatio = 0.70)
+train = subset(avocado_2017, sample == TRUE)
+test = subset(avocado_2017, sample == FALSE) 
+# Randomly split 2017 data into 70:30 training and testing datasets
 
-## Assessing missing data
+## Asessing missing data
 ## TRAINING DATASET 
+sapply(train, function(x) sum(is.na(x))) # Number missing by column
 sum(is.na(train)) 
 # 0 missing observations in training dataset
 
@@ -43,14 +54,7 @@ sum(is.na(train))
 sum(is.na(test)) 
 # 0 missing observations in testing dataset
 xTest <- test %>% dplyr::select(-AveragePrice) 
-
-## Running random forest in training dataset
-set.seed(150)  
-train.model <- randomForest(AveragePrice ~ ., data = train, mtry = 15, nodesize = 10) 
-# p/3 = 47 predictors/3 = 15 variables tried at each split
-# mtry reduces correlation between trees and improves accuracy
-# nodesize refers to the minimum number of observations at each split
-train.model
+# Retain all predictors and exclude outcome variable
 
 ## Running random forest in training dataset
 set.seed(1500)  
@@ -59,6 +63,7 @@ train.model.us <- randomForest(AveragePrice ~ ., data = train, mtry = 15, nodesi
 # mtry reduces correlation between trees and improves accuracy
 # nodesize refers to the minimum number of observations at each split
 train.model.us
+
 ## Evaluating random forest performance in training and testing datasets
 pred.train = predict(train.model.us, newdata = train)
 head(pred.train)
@@ -86,7 +91,7 @@ xTest %>% ggplot() +
   scale_y_continuous(breaks = seq(0, 3.25, 0.25)) + 
   xlab("Predicted avocado price in the US 2017 testing data (US dollars)") + 
   ylab("Observed avocado price in the US 2017 testing data (US dollars)") + 
-  ggtitle("Observed versus predicted avocado prices in the US testing data")
+  ggtitle("Observed versus predicted avocado prices in the US 2017 testing data")
 # Observed vs. predicted values in 2017 testing dataset 
 
 mean((pred.test - test$AveragePrice)^2) 
@@ -108,7 +113,6 @@ tmp %>% filter(Gini > 4.68) %>%
   theme(axis.text = element_text(size = 8))
 
 ## Range of avocado price predictions in US in 2017 by county
-
 avocado_2017_full <- avocado_clean %>%
   filter(year == 2017 & County != "roanoke" & County != "stlouis") %>% 
   dplyr::select(-"AveragePrice")
@@ -121,4 +125,3 @@ avocado_2017_full %>% ggplot(aes(x = factor(County), y = pred.2017)) +
   geom_boxplot(fill = "lightblue", alpha = 0.8) + 
   theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 12),
         axis.title.y = element_text(size = 12), plot.title =  element_text(size = 12))
-
